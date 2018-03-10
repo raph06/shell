2ed646d32cdfee79e10ec7909ec61bf8ab9229f7
Raphael <raphael.bonnet@etu.unice.fr>
2018-03-10 21:26:56 +0100

    history init

---
+++ hist/hist.sh
@@ -0,0 +1,85 @@
+#! /usr/bin/env bash
+
+usage="hist [-adcCmph] [path]\n-- program to save specific project history in a file called tips.txt \n\t-a --activate: Start the recording and signified by '>' prompt
+\n\t-d --deactivate: Stops the recording \n\t-c --clear: Clear unwanted variables (man,ls,echo...)  \n\t-C --Clear: Clear file
+\n\t-m --message: Add comment to the previous saved line \n\t-h --help: Display help \n\t[path]: Input specific path (Optionnal; Default '.')"
+status=""
+file='tips.txt'
+
+if [ "$#" -eq  "0" ]
+   then
+     echo "Error: no argument supplied"
+     echo -e $usage
+     return 1
+fi
+click=0
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    --activate|-a)
+    status=1
+    shift
+    ;;
+    --deactivate|-d)
+    status=0
+    shift
+    ;;
+    --clear|-c)
+    status=2
+    shift
+    ;;
+    --Clear|-C)
+    status=3
+    shift
+    ;;
+    --message|-m)
+    status=4
+    shift
+    ;;
+    --help|-h)
+    echo -e $usage
+    return 1
+    ;;
+    *)
+    echo ">>> Path supplied: $key"
+    path="$key"
+    click=1
+    shift
+    ;;
+esac
+done
+
+if [ "$status" == 1 ]
+  then
+    export PS1=$PS1'>'
+    if [ "$click" == 1 ]
+      then
+        file=$path$file
+        echo "$file"
+    fi
+    stamp_hours='#######Done on: '$(date +%d-%b-%H_%M)
+    echo -e $'\n'$stamp_hours$'\n' >> $file
+    echo $stamp_hours >> $file
+  #Ignore='/ls*/d; /exit/d; /pwd/d; /clear/d; /cd*/d; /man*/d; /more*/d; /less*/d; /head*/d; /tail*/d; /nano*/d; /open*/d; /source*/d'
+  #PROMPT_COMMAND='if history | tail -1 | grep -q 'cat\|ls\|echo'; then  echo "matched"; fi'
+  PROMPT_COMMAND='history | tail -1 >> "$file"'
+elif [ "$status" == 2 ]
+  then
+    echo 'clearing unwanted history'
+    sed -i.bak -e '/less/d;/ls/d;/man/d;/cat/d;/more/d;/clear/d;/echo/d;/help/d;/hist.sh.*/d;/test/d;/mkdir/d;/cp/d;/mv/d;/rm/d;/atom/d' $file
+elif [ "$status" == 3 ]
+  then
+    echo 'clearing file'
+    > $file
+elif [ "$status" == 4 ]
+   then
+    echo add a comment to $file
+    read varname
+    echo '>>>>'$varname >> $file
+elif [ "$status" == 0 ]
+  then
+    export PS1=$(echo $PS1 | sed 's/.\{1\}$//')
+    PROMPT_COMMAND=update_terminal_cwd
+fi
